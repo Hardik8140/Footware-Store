45a07662aa06a05bfa6b93a20897e24d7916bfc3
Hardik Gajera <hardik.gajera.d.84@gmail.com>
2023-03-29 08:18:56 +0530

    day-2 task

---
+++ Script/signin.js
@@ -0,0 +1,24 @@
+let Form = document.querySelector("form:nth-child(1)");
+let LSEmail = document.getElementById("email");
+let LSPass = document.getElementById("spass");
+
+let SData = JSON.parse(localStorage.getItem("Register")) || [];
+
+Form.addEventListener("submit", InpData);
+
+let flag = false;
+function InpData(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < SData.length; i++) {
+    if (LSEmail.value === SData[i].Email && LSPass.value === SData[i].Pass) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag) {
+    alert("yes");
+  } else {
+    alert("no");
+  }
+}
